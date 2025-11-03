361e4ffa4b6842b6c712d6a76113440b5339e8ca
lisandre-begon <lisandre.begon1@gmail.com>
2025-11-03 16:10:03 +0100

    Add type definitions for Booking,  and more generally stay service (Accomodation, Activity, LearningSkill, Meal, Review, and Stay)

---
+++ type/Booking.ts
@@ -0,0 +1,16 @@
+export interface Booking {
+    id_booking: number;
+    
+    stayId: number;
+    
+    backpackerId: number;
+
+    short_description: string;
+
+    request_date_start: Date;
+
+    request_date_end: Date;
+
+    status: 'pending' | 'accepted' | 'rejected' | 'cancelled';
+
+}
