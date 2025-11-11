68c662c949943d6d0e84740a07742b66c6cfdd9b
Yanis <yanis.baroudi@etu.umontpellier.fr>
2025-11-11 17:24:03 +0100

    screen woofshare completed

---
+++ data/woofshare.ts
@@ -0,0 +1,8 @@
+export const mockImages = [
+    require("../assets/images/woofshare1.png"),
+    require("../assets/images/woofshare2.png"),
+    require("../assets/images/woofshare3.png"),
+    require("../assets/images/woofshare4.png"),
+    require("../assets/images/woofshare5.png"),
+    require("../assets/images/woofshare6.png"),
+  ];
