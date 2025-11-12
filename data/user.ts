fe9874b472295957af80c1d2e1cefc157c49d3a8
Yanis <yanis.baroudi@etu.umontpellier.fr>
2025-11-12 16:11:19 +0100

    screen profile finished

--- data/user.ts
+++ data/user.ts
@@ -3,6 +3,6 @@ export const user = {
   email: "lucasgivre@gmail.com",
   password: "********",
   age: 22,
-  number: "+33 796576432",
+  number: "0796576432",
   address: "5 rue du Château, 23170 Crozant, France",
 };
