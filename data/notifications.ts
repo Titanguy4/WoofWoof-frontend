df1a2260368842ed3a7d1ae1eb611a2ae2f6de28
Yanis <yanis.baroudi@etu.umontpellier.fr>
2025-11-12 19:50:49 +0100

    screen notifications completed

---
+++ data/notifications.ts
@@ -0,0 +1,26 @@
+export const notifications = [
+  {
+    id: 1,
+    title: "Application accepted 🎉",
+    description:
+      "Congratulations! Your host has accepted your request for the mission at Brioude Farm.",
+    image: require("../assets/images/animalfarm.png"),
+    date: "2025-09-25",
+  },
+  {
+    id: 2,
+    title: "New mission available 🍓",
+    description:
+      "A project is looking for volunteers in Lyon to organize cultural events — apply now!",
+    image: require("../assets/images/communityevent.png"),
+    date: "2025-09-24",
+  },
+  {
+    id: 3,
+    title: "Review Request",
+    description:
+      "How was your mission? Share your feedback and help us improve!",
+    image: require("../assets/images/review-placeholder.png"),
+    date: "2025-09-23",
+  },
+];
