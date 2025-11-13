a7075391516df0c7f7f4154231503a2e2d7c1318
Yanis <yanis.baroudi@etu.umontpellier.fr>
2025-11-13 22:04:01 +0100

    all login screens completed

---
+++ app/changepassword.tsx
@@ -0,0 +1,15 @@
+// app/details/[id].tsx
+import { ChangePassword } from '@/screens';
+import { Stack } from "expo-router";
+import React from "react";
+
+export default function ChangePasswordPage() {
+
+
+  return (
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <ChangePassword/>
+    </>
+  );
+}
--- screens/index.tsx
+++ screens/index.tsx
@@ -1,9 +1,12 @@
 export { default as Backpackers } from './Backpackers';
+export { default as ChangePassword } from './ChangePassword';
+export { default as ChangePasswordSuccess } from './ChangePasswordSuccess';
 export { default as ChatScreen } from './ChatScreen';
 export { default as Details } from './Details';
 export { default as Discussion } from './Discussion';
 export { default as EditProfile } from './EditProfile';
 export { default as Explore } from './Explore';
+export { default as ForgotPassword } from './ForgotPassword';
 export { default as MissionRequest } from './MissionRequest';
 export { default as Missions } from './Missions';
 export { default as MyOffer } from './MyOffer';
@@ -12,6 +15,9 @@ export { default as Profile } from './Profile';
 export { default as RequestReceived } from './RequestReceived';
 export { default as RequestSuccess } from './RequestSuccess';
 export { default as Saved } from './Saved';
+export { default as SignIn } from './SignIn';
+export { default as SignUp } from './Signup';
 export { default as Woofshare } from './Woofshare';
+
 
 // ajoute ici d'autres écrans
